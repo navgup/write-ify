7c449e7ec3cfba6b83b73ed1c1eff58f306d6466
navgup <navgup@stanford.edu>
2023-08-25 22:11:20 -0400

    adding this

---
+++ docs/src/components/cta/CTA.jsx
@@ -0,0 +1,50 @@
+import React, {useState, useEffect} from 'react';
+import './cta.css';
+import InteractiveTextBox from './textbox.jsx';
+import { UseSelector } from 'react-redux';
+import axios from "axios";
+
+export const CTA = () => {
+
+  const [inputValue, setInputValue] = useState('');
+
+
+const sendText = async (text) => {
+  try {
+    const response = await axios.post('http://127.0.0.1:5000/predict', { text });
+    return response.data; 
+  } catch (error) {
+    console.error('Error processing text:', error);
+    return null;
+  }
+};
+
+useEffect(() => {
+    const intervalId = setInterval(() => {
+      // Check if the input value is not empty
+      if (inputValue.trim() !== '') {
+        sendText(inputValue) 
+          .then(result => {
+            if (result !== null) {
+              console.log('Result from ML model:', result);
+            }
+          })
+          .catch(error => {
+            console.error('Error processing text:', error);
+          });
+      }
+    }, 40000); // 60000 ms = 1 minute
+
+    return () => {
+      clearInterval(intervalId); 
+    };
+  }, [inputValue]);
+
+  return (
+    <div>
+      <InteractiveTextBox inputText={inputValue} onInputUpdate={setInputValue} />
+    </div>
+  )
+}
+
+export default CTA
